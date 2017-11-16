8529bf4d48e8557e82cde61bb8da1ab757757c9a
Chris <cchern.vaxd@gmail.com>
2017-11-16 14:51:52 +0800

    Add webhook.py

---
+++ webhook.py
@@ -0,0 +1,14 @@
+import requests
+import JSON
+
+url = "https://platform.uipath.com/api/Account/Authenticate"
+payload = "{\"TenancyName\":\"sriharishtenancy\",\"UsernameOrEmailAddress\":\"sriharish.gutta@accenture.com\",\"Password\":\"Harish613\"}"
+response = request.request("POST", url, data=payload)
+
+access_token = response.text['result']
+
+url = "https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs"
+
+header = "{\"Content-Type\": \"application/json\",\"Authorization\":\"access_token\"}"
+payload = "{\"startInfo\":{\"RobotIds\":[6996],\"ReleaseKey\":\"e70e3034-05f0-43bd-a101-f045f80bc1a7\",\"Strategy\":\"Specific\",\"NoOfRobots\":1,\"Source\":\"Manual\"}}"
+response = requests.request("POST", url, data=payload, headers=header)
