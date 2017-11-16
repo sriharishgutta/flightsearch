82deb3d154060e3c7f770417f8db06cac0e3d78c
Chris <cchern.vaxd@gmail.com>
2017-11-16 15:56:57 +0800

    Edited the json conversion code

--- webhook.py
+++ webhook.py
@@ -3,15 +3,13 @@ import json
 
 url = "https://platform.uipath.com/api/Account/Authenticate"
 payload = {"TenancyName":"sriharishtenancy","UsernameOrEmailAddress":"sriharish.gutta@accenture.com","Password":"Harish613"}
-response = request.request("POST", url, data=payload)
+response = requests.request("POST", url, data=payload)
 
-with open(response) as json_data:
-    data = json_data.read()
-    responseText = json.loads(data)
-access_token = response.text['result']
+responseText = json.loads(response.text)
+access_token = responseText['result']
 
 url = "https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs"
 
-header = {"Content-Type": "application/json","Authorization":"access_token"}
+header = {"Content-Type": "application/json","Authorization":access_token}
 payload = {"startInfo":{"RobotIds":[6996],"ReleaseKey":"e70e3034-05f0-43bd-a101-f045f80bc1a7","Strategy":"Specific","NoOfRobots":1,"Source":"Manual"}}
 response = requests.request("POST", url, data=payload, headers=header)
